ae443851e08bf1216195b8a4fc899b66cd4af8c3
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-02-02 16:39:01 -0300

    adicionei

---
+++ Desafios/d007.js
@@ -0,0 +1,7 @@
+let PI = 3.141592
+let raio = 5.9
+let area = PI * (raio * raio)
+console.log(`O resultado da área é: ${area}m`)
+
+area = Math.PI * raio * raio
+console.log(`O resultado da área é: ${area}m`)
