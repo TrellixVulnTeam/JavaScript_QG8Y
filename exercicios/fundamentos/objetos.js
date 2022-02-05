47ab54cc78efc4b6a90b2c421c07a0e25c530935
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-02-05 10:03:57 -0300

    atualizei

--- exercicios/fundamentos/objetos.js
+++ exercicios/fundamentos/objetos.js
@@ -4,3 +4,10 @@ prod1.preco = 9282.90
 prod1['Desconto legal'] = 0.50 //evitar atributos com espaço
 
 console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 98.90
+}
+
+console.log(prod2)
