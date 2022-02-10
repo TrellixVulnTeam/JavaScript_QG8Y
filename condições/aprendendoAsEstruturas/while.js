8684887048c9118517c6a09a5be0ecb9179eae26
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-02-10 08:24:54 -0300

    atualizei

---
+++ condições/aprendendoAsEstruturas/while.js
@@ -0,0 +1,10 @@
+function numeroAlatorio(min, max){
+    const valor = Math.random() * (max- min) + min
+    return Math.floor(valor)
+}
+let opcao = 0
+while (opcao != -1){
+    opcao = numeroAlatorio(-1, 10)
+    console.log(`O numero gerado foi ${opcao}.`)
+}
+console.log('Fim!')
