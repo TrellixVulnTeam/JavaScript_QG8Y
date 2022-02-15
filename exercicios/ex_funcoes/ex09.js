91c0f5b48317bba0064a4565714f1ec80ab79193
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-02-15 12:16:52 -0300

    mais um desafio

--- exercicios/ex_funcoes/ex09.js
+++ exercicios/ex_funcoes/ex09.js
@@ -1,10 +1,19 @@
-function nota(nota){
-    if(nota % 5 ==0){
-        return int(Math.floor(nota / 5)) * 5;
+function round(nota) {
+    if(nota >= 38){
+        if (nota % 5 != 0) {
+            const arredondamento = Math.ceil(nota / 5) * 5
+            if((arredondamento - nota) < 3){
+                return `Sua nota era ${nota} mas foi arredondada para ${arredondamento}`;
+            }else{
+                return `Sua nota não foi arredondada. Portanto, permanecerá ${nota}`
+            }
+        }else{
+            return `Sua nota é ${nota}`
+        }
     }else{
-        return (int(Math.floor(nota / 5))*5) + 5;
-
+        return `Você está reprovado com a nota ${nota}`
     }
 }
 
-console.log(nota(34))
+var n = 41;
+console.log(round(n)); 
