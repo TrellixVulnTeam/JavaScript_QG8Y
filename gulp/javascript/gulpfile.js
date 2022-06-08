7bd8411e2ae56263c712ac4dc15eba0aa5ed988c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-06-08 16:19:00 -0300

    tentando corrigir um bug

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -8,13 +8,13 @@ function padrao(cb){
     gulp.src('src/**/*.js')
     .pipe(babel({
         Comments: false, //vai tirar os comentarios
-        presets: ["env"] //busca a versão mais nova de js possivel no ncodigo
+        presets: ["@babel/env"], //busca a versão mais nova de js possivel no ncodigo
     }))// vai pegar tudo o que tiver mais de novo em js nos codigos js e vai transformar em uma versao mais antiga e compativel
 
     .pipe(uglify())//vai minificar os codigos selecionados 
     .on('error', err=> console.log(err))//caso aconteca algum evento ate aqui, nesse caso o evento é um erro, aciona a função que imprime o erro
     .pipe(concat('codigo.min.js'))//vai concatenar os codigos em um unico codigo
-    .pipe(gulp.dest('build/js'))
+    .pipe(gulp.dest('build'))
     return cb()
 }
 
