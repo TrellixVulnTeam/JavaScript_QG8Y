ac7f30c54d3a0bd036593fb62c7e84ac490ee484
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-06-09 17:04:57 -0300

    aprendizado de hoje
    corrigi o bug sobre a transformação js, aprendi sobre typescript e comecei sass

--- gulp/javascript/gulpfile.js
+++ gulp/javascript/gulpfile.js
@@ -4,18 +4,24 @@ const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const babel = require('gulp-babel')
 
-function padrao(cb){
+function transformacaoJS(cb){
     gulp.src('src/**/*.js')
     .pipe(babel({
-        Comments: false, //vai tirar os comentarios
-        presets: ["@babel/env"], //busca a versão mais nova de js possivel no ncodigo
+        Comments: false, 
+        //vai tirar os comentarios
+        presets: ["@babel/env"] 
+        //busca a versão mais nova de js possivel no ncodigo
+        
     }))// vai pegar tudo o que tiver mais de novo em js nos codigos js e vai transformar em uma versao mais antiga e compativel
 
     .pipe(uglify())//vai minificar os codigos selecionados 
     .on('error', err=> console.log(err))//caso aconteca algum evento ate aqui, nesse caso o evento é um erro, aciona a função que imprime o erro
     .pipe(concat('codigo.min.js'))//vai concatenar os codigos em um unico codigo
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build/js'))
     return cb()
 }
-
-exports.default = series(padrao)
+function fim(cb){
+    console.log('FIM!')
+    return cb()
+}
+exports.default = series(transformacaoJS, fim)
