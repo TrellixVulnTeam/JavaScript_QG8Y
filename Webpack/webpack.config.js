10c616a5c0abc378cde50771a2544a81f68b644b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-06-13 15:57:20 -0300

    Mais um exemplo
    aprendi a extrair o arquivo css que iria pra o arquivo js alterando a dom e fiz ele ir pra um arquivo css separado

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') // vai servir para extrair o css do arquivo js e coloca-lo em um arquivo css
 
 module.exports ={
     mode: 'development',
@@ -7,11 +8,17 @@ module.exports ={
         filename: 'principal.js',
         path:__dirname + '/public'
     },
+    plugins:[
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
     module:{
         rules:[{
             test: /\.css$/,
             use:[
-                'style-loader', //Adicionar CSS a DOM injetando a tag <style>
+                MiniCssExtractPlugin.loader,
+                //'style-loader', //Adicionar CSS a DOM injetando a tag <style>
                 'css-loader', //interpretar @import, url()..
             ]
         }]
