3022b7f495c9ed921620d03babdeb6f0a3977fed
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-06-13 11:14:15 -0300

    começando o capitulo webpack
    instalei as dependências do webpack e ainda fiz um exemplo de exportação e importação

---
+++ Webpack/src/principal.js
@@ -0,0 +1,5 @@
+import Pessoa from "./pessoa"; //Dessa maneira ainda nao é suportado entao vai dar erro
+import './modulos/moduloA'
+//const Pessoa = require('./pessoa')
+const atendente = new Pessoa
+console.log(atendente.cumprimentar())
