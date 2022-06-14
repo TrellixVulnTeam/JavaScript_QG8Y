4cbb3352990d134030160134d365c05330cebce7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-06-14 15:05:42 -0300

    aprendi a pegar imagens

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,7 +1,7 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') // vai servir para extrair o css do arquivo js e coloca-lo em um arquivo css
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports ={
@@ -13,9 +13,11 @@ module.exports ={
     },
     optimization:{
         minimizer:[
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
             }),
             new OptimizeCSSAssetsPlugin({})
         ]
@@ -34,6 +36,9 @@ module.exports ={
                 'css-loader', //interpretar @import, url()..
                 'sass-loader',
             ]
+        },{
+            test: /\.(png|svg|jpg|gif)$/,
+            use:['file-loader']
         }]
     }
 }
