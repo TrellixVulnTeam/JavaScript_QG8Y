66ec48c94ec2c7cba5a396d0f43cac80c06b4218
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-07-15 11:07:08 -0300

    Executando o Plugin para filtrar as fotos

--- bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
+++ bootstrap/projeto-galeria/src/js/plugins/cityButtons.js
@@ -1,40 +1,49 @@
 import $ from 'jquery'
-const duration = 600
 
-function filterByCity(city){
-    $('[wm-city]').each(function(i, e){
-        const isTarget = $(this).attr('wm-city') === city || city === null
-        if(isTarget){
+import { onLoadHtmlSuccess } from '../core/includes'
+
+const duration = 300
+
+function filterByCity(city) {
+    $('[wm-city]').each(function (i, e) {
+        const isTarget = $(this).attr('wm-city') === city
+            || city === null
+        if (isTarget) {
             $(this).parent().removeClass('d-none')
             $(this).fadeIn(duration)
-        }else{
+        } else {
             $(this).fadeOut(duration, () => {
                 $(this).parent().addClass('d-none')
             })
         }
     })
 }
-$.fn.cityButtons = function (){
+
+$.fn.cityButtons = function () {
     const cities = new Set
-    $('[wm-city]').each(function(i, e){
+    $('[wm-city]').each(function (i, e) {
         cities.add($(e).attr('wm-city'))
     })
-    
-    const btns = Array.from(cities).map(city =>{
-        const btn = $('<button>').addClass(['btn', 'btn-info']).html(city)
-        btn.on(e = filterByCity(city))
+
+    const btns = Array.from(cities).map(city => {
+        const btn = $('<button>')
+            .addClass(['btn', 'btn-info']).html(city)
+        btn.click(e => filterByCity(city))
         return btn
     })
-    const btnAll = $('<button>').addClass(['btn', 'btn-info', 'active']).html('Todas')
-    btnAll.on(e => filterByCity(null))
-    
+
+    const btnAll = $('<button>')
+        .addClass(['btn', 'btn-info', 'active']).html('Todas')
+    btnAll.click(e => filterByCity(null))
     btns.push(btnAll)
-    
+
     const btnGroup = $('<div>').addClass(['btn-group'])
     btnGroup.append(btns)
-    
+
     $(this).html(btnGroup)
     return this
 }
 
-$('[wm-city-buttons] ').cityButtons()
+onLoadHtmlSuccess(function() {
+    $('[wm-city-buttons]').cityButtons()
+})
