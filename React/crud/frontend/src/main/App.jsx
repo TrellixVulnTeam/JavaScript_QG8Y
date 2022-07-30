fa3e8e801bf243f187de4be15ce8ec70088796d4
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-07-30 10:21:53 -0300

    implementando estilo no header

--- React/crud/frontend/src/main/App.jsx
+++ React/crud/frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 import Logo from '../componentes/templates/Logo'
@@ -9,6 +11,6 @@ export default props =>
     <div className="app">
         <Logo/>
         <Nav/>
-        <Main/>
+        <Main icon="home" title="Inicio" subtitle="Segundo Projeto do capítulo de React." />
         <Footer/>
     </div>
