c61eb5b01abd9f7b19f08de54cba9fb37f3ce731
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-09-05 17:30:43 -0300

    Aplicando CSS modularizado

--- nextjs/fundamentos/src/components/Cabecalho.jsx
+++ nextjs/fundamentos/src/components/Cabecalho.jsx
@@ -1,7 +1,8 @@
-export default function Cabecalho(){
+export default function Cabecalho(props){
+    console.log(props)
     return (
         <header>
-            <h1>Fundamentos de Next.js & React</h1>
+            <h1>{props.titulo}</h1>
         </header>
     )
 }
--- nextjs/fundamentos/src/pages/exemplo.jsx
+++ nextjs/fundamentos/src/pages/exemplo.jsx
@@ -1,9 +1,12 @@
 import Cabecalho from "../components/Cabecalho";
-
+import Link from 'next/link'
 export default function Exemplo(){
     return(
         <>
-            <Cabecalho/>
+            <Cabecalho nome="xyz" idade={13} ehLegal={true} />
+            <Cabecalho titulo="Next.js & React" />
+            <Cabecalho titulo="Aprende Next na pratica" />
+            <Link href="/">Voltar</Link>
         </>
     )
 }
