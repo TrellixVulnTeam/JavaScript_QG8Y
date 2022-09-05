63f7d108be16d1573479ef77c36fb20b015f1e22
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-09-05 12:09:16 -0300

    dando continuidade ao estudo de next

---
+++ nextjs/fundamentos/src/pages/jsx.jsx
@@ -0,0 +1,18 @@
+export default function Jsx(){
+    const A = 4.9
+    const B = 7.3
+    console.log(A * B) 
+    const obj = {nome: 'Luan', idade: 20}
+    function subtitulo(){
+        return <h2>{"muito legal".toUpperCase()} </h2>
+    }
+    return(
+        <div>
+            <h1>JSX é um conceito central!</h1>
+            {subtitulo()}
+            <h2>{`Meu nome é ${obj.nome} e tenho ${obj.idade} anos`.toUpperCase()} </h2>
+            <p>{JSON.stringify({nome: 'Luan', idade: 20})} </p>
+
+        </div>
+    )
+}
