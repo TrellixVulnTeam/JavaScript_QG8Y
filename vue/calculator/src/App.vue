3544acc8c00c68bb045374c80e6c414051be13c5
Luan Alves <97049835+Luan029@users.noreply.github.com>
2022-10-15 10:43:15 -0300

    criando os componentes

--- vue/calculator/src/App.vue
+++ vue/calculator/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="app">
-        <h1>Olá Vue!</h1>
+        <h1>Calculadora</h1>
+        <Calculator />
     </div>
 </template>
 
 <script>
-export default {
+import Calculator from "./main/Calculator"
 
+export default {
+    components: { Calculator }
 }
 </script>
 
@@ -15,20 +18,24 @@ export default {
     font-family: "RobotoMono";
     src: url("./fonts/RobotoMono-Thin.ttf");
 }
-*{
+
+* {
     font-family: "RobotoMono", monospace;
 }
-body{
+
+body {
     margin: 0;
 }
-#app{
+
+#app {
     display: flex;
     flex-direction: column;
     height: 100vh;
     justify-content: center;
     align-items: center;
     text-align: center;
+
     color: #fff;
-    background-image: linear-gradient(rgb(83, 105,118), rgb(41, 46, 73));
+    background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
 }
 </style>
